0554021d78825cdd783a16be63d13262c8aef015
vic⚡ <builtbyvic@gmail.com>
2022-01-08 09:21:28 +0300

    initial transition

--- app.js
+++ app.js
@@ -1,3 +1,13 @@
 barba.init({
-  
-})
+  transitions: [{
+    name:'opacity-transition',
+    leave(data) {
+      // create your leave animation here
+      return gsap.to(data.current.container,{opacity:0});
+    },
+    enter(data) {
+      // create your enter animation here
+      return gsap.to(data.next.container,{opacity:1});
+    },
+  }]
+});
